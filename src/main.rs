9c380f305f5c7a5a1208e7f603a5d0da450afd0e
TheOnAndOnlyZenomat <eichelstein@online.de>
2021-02-15 21:56:38 +0100

    added documentation, added initial_multiplier function to update multiplier based on items

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+//! Rust Clicker game
+
 use std::io::{stdin, stdout, Read, Write};
-use std::thread; //these two line to sleep
+use std::thread;
 use std::time::Duration;
 use termion::{async_stdin, event::Key, input::TermRead, raw::IntoRawMode, terminal_size};
-extern crate termion;
 
+/// Stores all the information regarding the player, like to current points, his multiplier and his highscore (not yet implemented)
 #[derive(Debug)]
 struct Player {
     points: u128,
@@ -12,15 +14,23 @@ struct Player {
 }
 
 impl Player {
-    fn points_oneit(&mut self) {
-        self.points = self.points + self.multiplier
+    /// Update the mutliplier and take into consideration the amount of items. Useful to update the multiplier at launch
+    fn initial_multiplier(&mut self, item1: &Item) {
+        self.multiplier = item1.amount
     }
 
+    /// Updated the points counter by adding the multiplier (yes I know it's weird, that the multiplier gets added instead of multiplier...)
+    fn points_oneit(&mut self) {
+        self.points = self.points + self.multiplier;
+    }
+
+    /// Updates the multiplier with the given update parameter, can be used for the shop
     fn update_multiplier(&mut self, update: u128) {
-        self.multiplier = self.multiplier + update
+        self.multiplier = (self.multiplier + update)
     }
 }
 
+/// Stores all the information regarding an item, like name, price, multiplier and amount
 #[derive(Debug)]
 struct Item {
     name: String,
@@ -30,6 +40,7 @@ struct Item {
 }
 
 impl Item {
+    /// Handles buying an item. First checks, if player has enough points to buy, updates the multiplier, removes the price from the players points, increases the amount of the item
     fn buy(&mut self, mut playerstats: &mut Player) {
         if playerstats.points >= self.price {
             playerstats.update_multiplier(self.multiplier);
@@ -39,20 +50,25 @@ impl Item {
     }
 }
 
+/// exit function, which prints playerstats and item1 struct data for debug
 fn _exitdebug(playerstats: &Player, item1: &Item) {
     println!("{:?} \n {:?}", playerstats, item1);
     std::process::exit(1);
 }
 
+/// Function to exit the game, here stuff like saving will be handled (not yet implemented)
 fn exit(playerstats: &Player, item1: &Item) {}
 
 fn main() {
+    // initial setup
     let mut stdin = async_stdin().keys();
     let mut stdout = stdout().into_raw_mode().unwrap();
     let (termwidth, termheight) = terminal_size().unwrap(); //assigns the touple terinalwidth, terminalhight to the width and height of the terminal
+
+    // initializes the player
     let mut playerstats = Player {
-        points: 0,
-        multiplier: 0,
+        points: 100,
+        multiplier: 1,
         highscore: 0,
     };
 
@@ -64,21 +80,25 @@ fn main() {
         amount: 0,
     };
 
+    // update multiplier and take in consideration the amount of items
+    playerstats.initial_multiplier(&item1);
+
     // Displayloop
     loop {
         playerstats.points_oneit();
 
-        let multiplierlength = playerstats.multiplier.to_string().chars().count() as u16; // converts the multiplier u128 to a string, gets the chars, counts them and stores them as a u16, if multiplierlength is to big for a u16 it gets truncated
+        // chekcs the length of the multiplier and points as a string, so that we can use that to display the interface without cutting anything off
+        let multiplierlength = playerstats.multiplier.to_string().chars().count() as u16;
         let pointslength = playerstats.points.to_string().chars().count() as u16;
 
-        // this println prints the total number of points and the delta
+        // this println prints the total number of points, the delta and the shop
         writeln!(
             stdout,
             "{}{}Total: {}{}Delta: +{}{}1: {} ({}) - {}{}",
             termion::clear::All,         //clears the terminal screen
             termion::cursor::Goto(1, 1), // positions the cursor at column 1, line 1, prints the points in the top left corner
             playerstats.points,
-            termion::cursor::Goto(termwidth - (8 + multiplierlength + pointslength - 1), 1), // postions the cursor at column 80, line 1, prints the delta in the top right
+            termion::cursor::Goto(termwidth - (7 + multiplierlength + pointslength), 1), // postions the cursor at column 80, line 1, prints the delta in the top right
             playerstats.multiplier,
             termion::cursor::Goto(1, termheight),
             item1.name,
@@ -91,7 +111,7 @@ fn main() {
         if let Some(c) = stdin.next() {
             match c.unwrap() {
                 Key::Char('a') => playerstats.points += 1,
-                Key::Char('q') => break,
+                Key::Char('q') => _exitdebug(&playerstats, &item1),
                 Key::Char('1') => item1.buy(&mut playerstats),
                 //Key::Char('2') => (multiplier, counter) = shop('2', counter, multiplier),
                 _ => {}
